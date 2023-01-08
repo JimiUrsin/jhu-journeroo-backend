6e087ada1325563d0c01f3372d5970e456dccfeb
JimiUrsin <32343914+JimiUrsin@users.noreply.github.com>
2023-01-08 21:16:03 +0200

    Implemented pagination

--- src/main/kotlin/com/jhu/journeroo/controller/RestController.kt
+++ src/main/kotlin/com/jhu/journeroo/controller/RestController.kt
@@ -2,7 +2,10 @@ package com.jhu.journeroo.controller
 
 import com.jhu.journeroo.repository.JourneyRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Sort
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -10,6 +13,10 @@ class JourneyResource {
     @Autowired
     lateinit var journeyRepository: JourneyRepository
 
-    @GetMapping("/api/journeys")
-    fun listAll() = journeyRepository.findAllLimit()
+    @GetMapping("/api/journeys/{page}")
+    fun getPage(@PathVariable page: Int) =
+        journeyRepository.findPage(PageRequest.of(page, 30).withSort(Sort.by(Sort.Direction.ASC, "departureTimestamp")))
+
+    @GetMapping("/api/journeys/")
+    fun getAll() = journeyRepository.findAll(PageRequest.of(0, 30).withSort(Sort.by(Sort.Direction.ASC, "departureTimestamp")))
 }
--- src/main/kotlin/com/jhu/journeroo/repository/JourneyRepository.kt
+++ src/main/kotlin/com/jhu/journeroo/repository/JourneyRepository.kt
@@ -1,13 +1,13 @@
 package com.jhu.journeroo.repository
 
 import com.jhu.journeroo.model.Journey
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.CrudRepository
+import org.springframework.data.repository.PagingAndSortingRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface JourneyRepository : CrudRepository<Journey, Int> {
-    @Query("SELECT * FROM journeys LIMIT 10", nativeQuery = true)
-    fun findAllLimit() : Iterable<Journey>
-
+interface JourneyRepository : PagingAndSortingRepository<Journey, Int> {
+    @Query("SELECT * FROM journeys", nativeQuery = true)
+    fun findPage(pageable: Pageable): List<Journey>
 }
